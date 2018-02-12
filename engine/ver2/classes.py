6816464602885a8b2c07a8019b0cb82ad93a8e6d
Jackson Martin <jaja@gn.csd.local>
2018-02-12 13:38:27 -0500

    Save

--- engine/ver2/classes.py
+++ engine/ver2/classes.py
@@ -1,10 +1,11 @@
 class Thing():
-    hp = None
-    dmg = None
-    agi = None
-    dfc = None
-    mp = None
-    name=None
+    name = None #Name of Entity
+    hp = None #Health
+    dmg = None #Damage
+    agi = None #Agility
+    dfc = None #Defence
+    mp = None #Magic Power(How much health you gain)
+    mpu = None #Magic Power Usage(How many times you can heal
 
 class Player(Thing):
     gold = 0 
@@ -13,6 +14,19 @@ class Enemy(Thing):
     pass
 
 class DefaultPlayer(Player):
-    pass
+    hp = 10
+    dmg = 5
+    agi = 10
+    dfc = 2
+    mp = 1
+    mpu = 1
 
+class Slime(Enemy):
+    name = "Slime"
+    hp = 6
+    dmg = 3
+    agi = 5
+    dfc = 1
+    mp = 3
+    mpu = 1
 
--- engine/ver2/engine.py
+++ engine/ver2/engine.py
@@ -1,4 +1,5 @@
 import classes as cl
+import random
 
 def hpScan():
     if player.hp < 1:
@@ -21,24 +22,47 @@ def fight():
 def playerAttack():
     print("Player attacks!")
     enemy.hp -= player.dmg
+    enemy.hp += enemy.dfc
+
+def playerHeal():
+    if player.mpu < 1:
+        print("You are out of Magic!")
+        playerAttack()
+    else:
+        print("Player heals " + str(player.mp) + "HP!")
+        player.hp += player.mp
+        player.mpu -= 1
 
 def enemyAttack():
     print("Enemy atttacks!")
     player.hp -= enemy.dmg
-    
+    player.hp += player.dfc
+
+def enemyHeal():
+    if enemy.mpu < 1: #Checks to see if Magic Power Usage is less than 1
+        enemyAttack() #Attacks because Enemy can not heal
+    else:
+        print("Enemy heals " + str(enemy.hp) + " HP!")
+        enemy.hp += enemy.mp 
+        enemy.mpu -= 1
+
 def playerMove():        
-    print("Player HP: " + str(player.hp))
-    print("Enemy HP: " + str(enemy.hp))
-    prompt = input("Attack(1) >>>").strip()
+    print("Player HP: " + str(player.hp) + " Player MP: " + str(player.mpu))
+    print("Enemy HP: " + str(enemy.hp) + " Enemy MP: " + str(enemy.mpu))
+    prompt = input("Attack(1), Heal(2) >>>").strip()
     if prompt == "1":
         playerAttack()
+    elif prompt == "2":
+        playerHeal()
     else:
         print("You were confused!")
 
-
 def enemyMove():
-    enemyAttack()
-    #add more later
+    move = random.randint(1, 2)
+    if move == 1:
+        enemyAttack()
+    elif move == 2:
+        enemyHeal()
                 
 def run():
     global player, monster
@@ -46,6 +70,8 @@ def run():
         fight()
 
 if __name__ == "__main__":
+    cl.Player = cl.DefaultPlayer
+    cl.Enemy = cl.Slime
     player = cl.Player()
     enemy = cl.Enemy()
     run()
